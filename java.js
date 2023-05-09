37a9d710156826256ee73c8a535b78e69f38d4f7
Calebcmf <caleb_freitas@estudante.sesisenai.org.br>
2023-05-09 10:21:28 -0300

    caleb

--- java.js
+++ java.js
@@ -4,6 +4,7 @@ document.onkeydown = Pega_Tecla;
         e = e || window.event;
     
         tecla = e.keyCode;
-        idtecla = console.log(tecla); // MOSTRA O CÓDIGO DA TECLA PRESSIONADA
-        idtecla(display = "none")
+        id = console.log(tecla); // MOSTRA O CÓDIGO DA TECLA PRESSIONADA
+        var idtecla = document.getElementById(id)
+        idtecla.add.document = "none"
     }
